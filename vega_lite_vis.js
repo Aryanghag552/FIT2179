1245236694f251e4fddfef0ba86a134a944c748b
Aryanghag552 <agha0030@student.monash.edu>
2026-06-03 21:37:36 +1000

    Update chart 4 and 5 vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -15,3 +15,15 @@ vegaEmbed("#bubble_plot", vg_3).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "heatmap.vg.json";
+vegaEmbed("#heatmap", vg_4).then(function(result) {
+    // Access the Vega view instance
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "line_brush.vg.json";
+vegaEmbed("#line_brush", vg_5).then(function(result) {
+    // Access the Vega view instance
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

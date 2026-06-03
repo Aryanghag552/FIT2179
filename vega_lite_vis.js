6de4f4ec794189b07ae5a5f01b1e5d4edbd8ac86
Aryanghag552 <agha0030@student.monash.edu>
2026-06-03 21:29:46 +1000

    Update chart 3 vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -9,3 +9,9 @@ vegaEmbed("#choropleth_map", vg_2).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_3).then(function(result) {
+    // Access the Vega view instance
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

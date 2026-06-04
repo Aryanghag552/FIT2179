8a19a51d0f7b574c89fa021423d5b579f1982643
Aryanghag552 <agha0030@student.monash.edu>
2026-06-04 19:47:38 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,71 +1,73 @@
+var embedOpt = {"actions": false};
+
 var vg_1 = "symbol_map.vg.json";
-vegaEmbed("#symbol_map", vg_1).then(function(result) {
+vegaEmbed("#symbol_map", vg_1, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "choropleth_map.vg.json";
-vegaEmbed("#choropleth_map", vg_2).then(function(result) {
+vegaEmbed("#choropleth_map", vg_2, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "bubble_plot.vg.json";
-vegaEmbed("#bubble_plot", vg_3).then(function(result) {
+vegaEmbed("#bubble_plot", vg_3, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "heatmap.vg.json";
-vegaEmbed("#heatmap", vg_4).then(function(result) {
+vegaEmbed("#heatmap", vg_4, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "line_brush.vg.json";
-vegaEmbed("#line_brush", vg_5).then(function(result) {
+vegaEmbed("#line_brush", vg_5, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_6 = "stacked_area.vg.json";
-vegaEmbed("#stacked_area", vg_6).then(function(result) {
+vegaEmbed("#stacked_area", vg_6, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_7 = "grouped_bar.vg.json";
-vegaEmbed("#grouped_bar", vg_7).then(function(result) {
+vegaEmbed("#grouped_bar", vg_7, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_8 = "diverging_bar.vg.json";
-vegaEmbed("#diverging_bar", vg_8).then(function(result) {
+vegaEmbed("#diverging_bar", vg_8, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_9 = "coral_cover_line.vg.json";
-vegaEmbed("#coral_cover_line", vg_9).then(function(result) {
+vegaEmbed("#coral_cover_line", vg_9, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_10 = "donut_chart.vg.json";
-vegaEmbed("#donut_chart", vg_10).then(function(result) {
+vegaEmbed("#donut_chart", vg_10, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_11 = "slope_chart.vg.json";
-vegaEmbed("#slope_chart", vg_11).then(function(result) {
+vegaEmbed("#slope_chart", vg_11, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_12 = "small_multiples.vg.json";
-vegaEmbed("#small_multiples", vg_12).then(function(result) {
+vegaEmbed("#small_multiples", vg_12, embedOpt).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
